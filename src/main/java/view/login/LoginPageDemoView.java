aa8088c34e97b11321456c1e621a06f28bcd7044
Hasan Ahmed <hasan15ahmed28@gmail.com>
2025-11-17 15:37:31 -0500

    Fixed Basic LoginPageDemo for testing purposes

--- src/main/java/view/login/LoginPageDemoView.java
+++ src/main/java/view/login/LoginPageDemoView.java
@@ -1,4 +1,19 @@
 package view.login;
 
+import javax.swing.*;
+
 public class LoginPageDemoView {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Recipe App");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // our login panel
+            frame.setContentPane(new LoginPage());
+
+            frame.pack();                 // size window to fit components
+            frame.setLocationRelativeTo(null); // center on screen
+            frame.setVisible(true);
+        });
+    }
 }
