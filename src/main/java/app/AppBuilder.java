8ee7b7f5e4953ee71004eaa4cfcc80ee91064bd6
Hasan Ahmed <hasan15ahmed28@gmail.com>
2025-11-24 15:54:59 -0500

    Started implementing AppBuilder with basic signup page, login page, and home page.

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -1,5 +1,47 @@
 package app;
 
-public class AppBuilder {
+import interface_adapter.ViewManagerModel;
+import view.FridgeView;
+import view.HomeView;
+import view.LoginView;
+import view.SignUpView;
+import view.ViewManager;
+import view.ViewRecipeView;
 
+public class AppBuilder {
+    private final ViewManagerModel viewManagerModel;
+    private final ViewManager viewManager;
+
+    private LoginView loginView;
+    private SignUpView signUpView;
+    private HomeView homeView;
+    private FridgeView fridgeView;
+    private ViewRecipeView viewRecipeView;
+
+    public AppBuilder() {
+        viewManagerModel = new ViewManagerModel();
+        viewManager = new ViewManager(viewManagerModel);
+    }
+
+    public AppBuilder addLoginView() {
+        loginView = new LoginView(viewManagerModel);
+        viewManager.addView(loginView, loginView.getViewName());
+        return this;
+    }
+
+    public AppBuilder addSignUpView() {
+        signUpView = new SignUpView(viewManagerModel);
+        viewManager.addView(signUpView, signUpView.getViewName());
+        return this;
+    }
+    public AppBuilder addHomeView() {
+        homeView = new HomeView(viewManagerModel);
+        viewManager.addView(homeView, homeView.getViewName());
+        return this;
+    }
+    
+    public void show() {
+        viewManagerModel.setActiveViewName(loginView.getViewName());
+        viewManager.show();
+    }
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,17 @@
 package app;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            AppBuilder builder = new AppBuilder();
+            builder
+                    .addLoginView()
+                    .addSignUpView()
+                    .addHomeView()
+                    .show();
+        });
+    }
 
 }
