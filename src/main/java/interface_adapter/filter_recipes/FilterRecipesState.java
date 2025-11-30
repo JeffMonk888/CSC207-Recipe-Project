56b5bf509d0600be0b5021e4a343431eaacf9737
Hasan Ahmed <hasan15ahmed28@gmail.com>
2025-11-30 11:51:18 -0500

    Part of previous, forgot to commit.

--- src/main/java/interface_adapter/filter_recipes/FilterRecipesState.java
+++ src/main/java/interface_adapter/filter_recipes/FilterRecipesState.java
@@ -1,4 +1,18 @@
 package interface_adapter.filter_recipes;
 
+import domain.entity.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FilterRecipesState {
+    private List<Recipe> results = new ArrayList<>();
+
+    public List<Recipe> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Recipe> results) {
+        this.results = results;
+    }
 }
